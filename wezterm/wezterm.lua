1b245d0937edf16f0df05bb19484ab46878b27d5
Mark Nichols <mhn@ksu.edu>
2026-04-29 08:17:09 -0500

    bumped wezterm font to 16

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -28,7 +28,7 @@ config.scrollback_lines = 10000
 -- fonts
 -- config.font = wezterm.font 'JetBrainsMonoNL Nerd Font'
 config.font = wezterm.font("Intel One Mono", { weight = "Regular", italic = false })
-config.font_size = 15
+config.font_size = 16
 config.line_height = 1.05
 
 -- tabs
